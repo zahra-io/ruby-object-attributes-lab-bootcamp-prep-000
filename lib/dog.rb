066f3ecc83079f7664eb6e706615189877f7bb04
Zahra Khan <zarssskhan@gmail.com>
2017-10-12 06:05:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,17 @@
 class Dog
+  def name
+    @name
+  end
 
-def name
-  @name
-end
+  def name=(dogs_name)
+    @name = dogs_name
+  end
 
-def name=(dogs_name)
-  @name = dogs_name
-end
+  def breed
+    @breed
+  end
 
-def breed
-  @breed
-end
-
-def breed=(dogs_breed)
-  @breed = dogs_breed
-end 
-
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end 
 end
